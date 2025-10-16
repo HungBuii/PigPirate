7148f8c772568cd113ea57dc7ebbfcfb5c67cf3d
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-10-16 14:21:46 +0700

    update assets

---
+++ Source/PigPirate/PigPirate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PigPirate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PigPirate, "PigPirate" );
